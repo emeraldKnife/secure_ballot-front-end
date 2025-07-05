8ae5b3662d386cb58c83bee8b3163911b1884372
MOHIT PATIL <mohitypatil2006@gmail.com>
2025-07-05 19:28:46 +0530

    Update App.js

--- secure_ballot/src/App.js
+++ secure_ballot/src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Voters from './pages/Voters';
+import Vote from './pages/Vote';
+import Votes from './pages/Votes';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/voters" element={<Voters />} />
+        <Route path="/vote" element={<Vote />} />
+        <Route path="/votes" element={<Votes />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
